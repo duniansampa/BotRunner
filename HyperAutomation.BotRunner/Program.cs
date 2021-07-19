e7e01da54ec60a13715b4cd569b208b577ace252
duniansampa <duniansampa@outlook.com>
2021-07-18 21:00:46 -0300

    - First commit

---
+++ HyperAutomation.BotRunner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HyperAutomation.BotRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
